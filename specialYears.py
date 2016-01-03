5181a1c563323729bea839c351a386469a5905b2
Frederik Stensaeth <stensaethf@carleton.edu>
2016-01-03 17:44:08 +0100

    specialYears.py added

---
+++ specialYears.py
@@ -0,0 +1,12 @@
+'''
+specialYears.py
+Frederik Roenn Stensaeth
+01.03.15
+
+An investigation into whether some years are more special than others.
+
+To Do:
+- Divisors.
+- Dynamic programming solution.
+- Which is faster?
+'''
